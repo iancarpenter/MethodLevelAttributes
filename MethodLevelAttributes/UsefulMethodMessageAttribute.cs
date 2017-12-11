9f480224222581d1be817fae6e3a1250dfbbfaae
Ian Carpenter <ianhcarpenter@gmail.com>
2017-12-11 11:05:20 +0000

    BroughtUptoDate

--- MethodLevelAttributes/UsefulMethodMessageAttribute.cs
+++ MethodLevelAttributes/UsefulMethodMessageAttribute.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MethodLevelAttributes
 {
     [AttributeUsage(AttributeTargets.Method)]
-    class UsefulMethodMessageAttribute : Attribute
+    public sealed class UsefulMethodMessageAttribute : Attribute
     {
         string s;
 
